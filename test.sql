ea5ab00933ea3ae398f0d47ac4b0816bc5d75d8d
Joe Valookaran <joevalookaran@hotmail.com>
2022-02-08 21:25:46 +0000

    finished the fourth round or queries

--- test.sql
+++ test.sql
@@ -1,14 +1,23 @@
-SELECT students.name as student, count(assignment_submissions.*) as total_submissions
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-WHERE students.end_date IS NULL
-GROUP BY students.name
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name
 
--- select in select
+-- -- select in select
 
-SELECT (
-  SELECT count(*)
-  FROM table_name
-) as total, other_column
-FROM other_table_name
-ORDER BY total;
+-- SELECT (
+--   SELECT count(*)
+--   FROM table_name
+-- ) as total, other_column
+-- FROM other_table_name
+-- ORDER BY total;
+
+SELECT *
+FROM (SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC) AS averageCohortTime
+LIMIT 1;
